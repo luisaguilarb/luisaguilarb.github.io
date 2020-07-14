244bf47ada64782b5b9bbad3fd173dd970d76d41
luisaguilarb <luisaguilarbote@gmail.com>
2020-07-14 15:57:27 -0600

    Updates

---
+++ precache-manifest.21b7846a635254fca9cc6e8301c1fb16.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798299940fea3b5b6e0e5aa0077dab82",
+    "url": "/udemy-react2020/index.html"
+  },
+  {
+    "revision": "92fe919ae08fef7ac55a",
+    "url": "/udemy-react2020/static/css/main.b5be6306.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/udemy-react2020/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/udemy-react2020/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92fe919ae08fef7ac55a",
+    "url": "/udemy-react2020/static/js/main.8ae3ebcd.chunk.js"
+  },
+  {
+    "revision": "3faddd09fa372b532481",
+    "url": "/udemy-react2020/static/js/runtime-main.4c0611b4.js"
+  }
+]);
